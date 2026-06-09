f0fd951dd775e7ef7b19a83a1be25f2227b0efa3
Funke Arinola <funkearinola5@gmail.com>
2026-06-09 23:38:35 +0100

    Fix plugin loader declaration in settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -18,7 +18,7 @@ herepluginManagement {
 }
 
 plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0" apply false
 
     id("com.android.application") version "8.6.0" apply false
 
